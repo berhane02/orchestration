5645e3b6630112b4c82a53932b7bdff25764f68d
berhane02 <yberhane1@outlook.com>
2024-01-08 23:02:37 -0500

    add backend for eks

--- jenkins-pipeline-deploy-k8/terraform/backend.tf
+++ jenkins-pipeline-deploy-k8/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
-    backend "s3" {
-        bucket = "automation-jenkis-pipeline"
-        region = "us-east-1"
-        key = "jenkis-server/terraform.tfstate"
-    }
+  backend "s3" {
+    bucket = "primuslearning-app"
+    region = "us-east-1"
+    key = "eks/terraform.tfstate"
+  }
 }
